07c607689a7f29e319a488f449decf7835ecabd4
Byron Sommardahl <byron@acklenavenue.com>
2012-08-12 14:00:07 -0500

    Renamed file.

--- DevTests.CSharp.StringCalculator/when_calculating_strings.cs
+++ DevTests.CSharp.StringCalculator/when_calculating_strings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using Machine.Specifications;
+﻿using Machine.Specifications;
 
 namespace DevTests.CSharp.StringCalculator
 {
